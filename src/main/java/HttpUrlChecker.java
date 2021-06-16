07d83664309d3205e5790c9f45646cd243464e35
Johannes SCHWEIGER <55189367+JSchweiger2703@users.noreply.github.com>
2021-06-16 16:11:18 +0200

    1st Unit Test

--- src/main/java/HttpUrlChecker.java
+++ src/main/java/HttpUrlChecker.java
@@ -21,3 +21,13 @@
  * PROVIDED HEREUNDER IS PROVIDED "AS IS". HTBLA LEONDING HAS NO OBLIGATION
  * TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HttpUrlChecker {
+    public static Matcher getMatcher(String httpString) {
+        Pattern pattern = Pattern.compile("^(http)://[a-zA-Z0-9]+.[a-zA-Z]+");
+        return pattern.matcher(httpString);
+    }
+}
